010a5fd9dec42463dfe6bdc8043713bfb0fce54c
Krovinskii <181596453+krovinskii@users.noreply.github.com>
2024-12-17 18:36:50 -0600

    grid is dynamic

--- script.js
+++ script.js
@@ -1,26 +1,37 @@
-//Create default grid at 16, user is able to change this
+// Create default grid at 16, user is able to change this
 const createGrid = (amountOfDivs) => {
-    document.querySelectorAll(".createdDivs").forEach(div => div.remove());
-    for (let i=0; i < amountOfDivs; i++) {
-        const grabMainDiv = document.getElementById("htmlMainBox");
+    const grabMainDiv = document.getElementById("htmlMainBox");
+    document.querySelectorAll(".createdDivs").forEach(div => div.remove()); //remove old boxes
+    const divsInRow = Math.ceil(Math.sqrt(amountOfDivs)); // Try to make it square-like
+    const containerWidth = grabMainDiv.clientWidth;
+
+    const divSize = (containerWidth / divsInRow) + "px"; 
+
+    // Create the requested number of divs
+    for (let i = 0; i < amountOfDivs; i++) {
         const createDiv = document.createElement("div");
         createDiv.classList.add("createdDivs");
         createDiv.innerText = "hello";
-        htmlMainBox.appendChild(createDiv);
+        createDiv.style.width = divSize; 
+        createDiv.style.height = divSize; 
+        grabMainDiv.appendChild(createDiv);
     }
 };
-//On load, create 16 divs. 
+
+// On load, create 16 divs (default)
 createGrid(16);
 
 const userChangeGrid = document.getElementById("userChangeGrid");
 userChangeGrid.addEventListener("click", () => {
-    const askGrid = prompt("Enter new grid size:");
-    if (askGrid > 15) {
-        alert("Please enter a number less than 15")
-        //How do I call askGrid again right after the alert?
+    let askGrid = prompt("Enter new grid size:");
+    askGrid = parseInt(askGrid);
+    while (isNaN(askGrid) || askGrid <= 0 || askGrid > 150) {
+        if (askGrid > 150) {
+            alert("Please enter a number less than 150");
+        } else if (isNaN(askGrid) || askGrid <= 0) {
+            alert("Please enter a valid number");
+        }
+
     }
-    const checkIfNumber = (isNaN(askGrid)) ? alert("Please enter a number") : false; // Ternary for reduced clutter, better readability.. 
-    console.log(askGrid);
     createGrid(askGrid);
-    //Need to take users input and pass it to this function
 });
